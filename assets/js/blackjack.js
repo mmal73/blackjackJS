22e45de10852051ffdda1b8dbf665b28cf0b440f
mmal73 <luisito250899@gmail.com>
2022-04-02 15:50:56 -0600

    feat: finished game logic and DOM events

--- assets/js/blackjack.js
+++ assets/js/blackjack.js
@@ -7,9 +7,12 @@ let playerPoints = 0,
   oponentPoints = 0;
 
 /* Html references */
+const btnNewGame = document.querySelector('#btnNewGame');
 const btnHit = document.querySelector('#btnHit');
+const btnStand = document.querySelector('#btnStand');
 const pointsContainer = document.querySelectorAll('small');
 const playerCardsContainer = document.querySelector('#player-cards');
+const oponentCardsContainer = document.querySelector('#oponent-cards');
 
 const shuffle = (array) => {
   let currentIndex = array.length,
@@ -26,6 +29,18 @@ const shuffle = (array) => {
   }
 
   return array;
+};
+
+const startGame = () => {
+  btnHit.disabled = false;
+  btnStand.disabled = false;
+  playerPoints = 0;
+  oponentPoints = 0;
+  pointsContainer[0].innerText = 0;
+  pointsContainer[1].innerText = 0;
+  playerCardsContainer.innerHTML = '';
+  oponentCardsContainer.innerHTML = '';
+  deck = createDeck();
 };
 
 const createDeck = () => {
@@ -53,15 +68,55 @@ const cardValue = (card) => {
   return isNaN(cardValue) ? (cardValue == 'A' ? 11 : 10) : Number(cardValue);
 };
 
-deck = createDeck();
+const cardImg = (currentCard) => {
+  const img = document.createElement('img');
+  img.src = `assets/cards/${currentCard}.png`;
+  img.classList.add('card');
+  return img;
+};
+
+const oponentHit = () => {
+  do {
+    const currentCard = hit();
+    const img = cardImg(currentCard);
+    oponentPoints += cardValue(currentCard);
+    pointsContainer[1].innerText = oponentPoints;
+    oponentCardsContainer.append(img);
+    if (playerPoints > 21) {
+      break;
+    }
+  } while (oponentPoints <= playerPoints && playerPoints <= 21);
+  if (
+    (oponentPoints > playerPoints && oponentPoints <= 21) ||
+    playerPoints > 21
+  ) {
+    alert('Oponent Win');
+  } else {
+    alert('Player Win');
+  }
+};
 
 btnHit.addEventListener('click', () => {
   const currentCard = hit();
   playerPoints += cardValue(currentCard);
-  pointsContainer[0].innerHTML = playerPoints;
-
-  const cardImg = document.createElement('img');
-  cardImg.src = `assets/cards/${currentCard}.png`;
-  cardImg.classList.add('card');
-  playerCardsContainer.append(cardImg);
+  pointsContainer[0].innerText = playerPoints;
+  const img = cardImg(currentCard);
+  playerCardsContainer.append(img);
+  if (playerPoints > 21) {
+    btnHit.disabled = true;
+    oponentHit();
+  } else if (playerPoints == 21) {
+    alert('Player 1 Win');
+  }
 });
+
+btnStand.addEventListener('click', () => {
+  btnHit.disabled = true;
+  btnStand.disabled = true;
+  oponentHit();
+});
+
+btnNewGame.addEventListener('click', () => {
+  startGame();
+});
+startGame();
